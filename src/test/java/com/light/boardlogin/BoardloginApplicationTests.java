845c70eade3346258288576b0356989195440096
light10 <kwangyeol3@gmail.com>
2022-07-20 20:40:41 +0700

    init

---
+++ src/test/java/com/light/boardlogin/BoardloginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.light.boardlogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardloginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
